127a701073bb6aa48eb1ff67ade0d47c8f247b76
Dmitry Polyak <dpolyak@outbrain.com>
2018-07-15 22:18:35 +0800

    added minor changes to prices simple block

--- cripto/views.py
+++ cripto/views.py
@@ -4,16 +4,13 @@ def home(request):
 	import requests
 	import json
 
-	# Grab crypto price date
-	price_api_request = requests.get("https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,REP,DASH,IOT&tsyms=USD")
-	price_api = json.loads(price_api_request.content)
+	# Grab crypto multi price date
+	price_multi_api_request = requests.get("https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,REP,DASH,IOT&tsyms=USD")
+	price_multi_api = json.loads(price_multi_api_request.content)
 
 
 	# Grab crypto news
 	news_api_request = requests.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
 	news_api = json.loads(news_api_request.content)
 
-	return render(request, 'home.html', {'news': news_api, 'price': price_api})
-
-def contact_us(request):
-	return render(request, 'contacts.html', {})
+	return render(request, 'home.html', {'news': news_api, 'price': price_multi_api})

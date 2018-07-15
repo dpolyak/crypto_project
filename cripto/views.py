e2c17e6856d4e76e6e4956ca160022e66d95764b
Dmitry Polyak <dpolyak@outbrain.com>
2018-07-15 23:07:32 +0800

    added full price bolck instaed of multy price (ch. 16)

--- cripto/views.py
+++ cripto/views.py
@@ -4,13 +4,13 @@ def home(request):
 	import requests
 	import json
 
-	# Grab crypto multi price date
-	price_multi_api_request = requests.get("https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,REP,DASH,IOT&tsyms=USD")
-	price_multi_api = json.loads(price_multi_api_request.content)
+	# Grab crypto full price data
+	price_full_api_request = requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,XRP,ETH,REP,DASH,IOT&tsyms=USD")
+	price_full_api = json.loads(price_full_api_request.content)
 
 
 	# Grab crypto news
 	news_api_request = requests.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
 	news_api = json.loads(news_api_request.content)
 
-	return render(request, 'home.html', {'news': news_api, 'price': price_multi_api})
+	return render(request, 'home.html', {'news': news_api, 'price_full': price_full_api})
